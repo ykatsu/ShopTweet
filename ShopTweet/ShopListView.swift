4a34a630a70d39336c99bbd2ed582e2be56f4003
Yoshihisa Katsumata <ykatsu@users.noreply.github.com>
2015-01-20 18:00:02 +0900

    Show shopView by navigationcontroller

--- ShopTweet/ShopListView.swift
+++ ShopTweet/ShopListView.swift
@@ -12,6 +12,11 @@ class ShopListView: UIViewController, UITableViewDelegate, UITableViewDataSource
 
     @IBOutlet weak var tableView: UITableView!
     
+    let imgArray: NSArray = ["image2.JPG","image3.JPG","image2.JPG","image3.JPG"]
+    let label2Array: NSArray = ["image0","image1","image2","image3"]
+    
+    var selectedRow: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,7 +26,7 @@ class ShopListView: UIViewController, UITableViewDelegate, UITableViewDataSource
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return imgArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -33,7 +38,24 @@ class ShopListView: UIViewController, UITableViewDelegate, UITableViewDataSource
         return cell
     }
     
+    // Cell が選択された場合
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath!) {
+        selectedRow = indexPath.row
+        
+        // ShopController へ遷移するために Segue を呼び出す
+        performSegueWithIdentifier("pushToShop",sender: nil)
+    }
     
+    // Segue 準備
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "pushToShop") {
+            let subVC: ShopViewController = segue.destinationViewController as ShopViewController
+            // [indexPath.row] から画像名を設定
+            subVC.shopImageName = "\(imgArray[selectedRow])"
+            subVC.shopLabelText = "\(label2Array[selectedRow])"
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- ShopTweet/ShopViewController.swift
+++ ShopTweet/ShopViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ShopViewController: UIViewController {
 
+    @IBOutlet weak var shopImage: UIImageView!
+    @IBOutlet weak var shopLabel: UILabel!
+    
+    var shopImageName: String!
+    var shopLabelText: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        shopImage.image = UIImage(named:shopImageName)
+        // 画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
+//        shopImage?.contentMode = UIViewContentMode.ScaleAspectFit
+        shopLabel.text = shopLabelText
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 }
