c4a3dc4eb38dbab8aa5448545a14eeaa550e3329
Yoshihisa Katsumata <ykatsu@users.noreply.github.com>
2015-01-20 15:24:18 +0900

    Before NavigationController

--- ShopTweet/ShopListView.swift
+++ ShopTweet/ShopListView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ShopListView: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
---
+++ ShopTweet/ShopViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ShopViewController.swift
+//  ShopTweet
+//
+//  Created by Yoshihisa Katsumata on 2015/01/20.
+//  Copyright (c) 2015年 Yoshihisa Katsumata. All rights reserved.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
