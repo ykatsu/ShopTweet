f161f4bd35e488776048a28643d5f4404a44e040
Yoshihisa Katsumata <ykatsu@users.noreply.github.com>
2015-01-20 18:16:31 +0900

    ShopVewにTableView追加

--- ShopTweet/ShopViewController.swift
+++ ShopTweet/ShopViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ShopViewController: UIViewController {
+class ShopViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var shopImage: UIImageView!
     @IBOutlet weak var shopLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var shopImageName: String!
     var shopLabelText: String!
@@ -20,12 +21,28 @@ class ShopViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        tableView.delegate = self
+        tableView.dataSource = self
+
         shopImage.image = UIImage(named:shopImageName)
         // 画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
 //        shopImage?.contentMode = UIViewContentMode.ScaleAspectFit
         shopLabel.text = shopLabelText
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("tweetTableCell", forIndexPath: indexPath) as UITableViewCell
+        
+        var textView = tableView.viewWithTag(11) as UITextView
+        textView.text = "ほげ\(indexPath.row+1)"
+        
+        return cell
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
